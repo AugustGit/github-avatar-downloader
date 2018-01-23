db9e61324e68f94f08b66592de053ba6fd3cad11
austin metka <austin_metka@hotmail.com>
2018-01-23 02:43:55 +0000

    problem with function downloadImageByURL(url, filePath) {
      request.get(url)
    
      // ...
    }

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,8 @@
 var request = require('request');
 
-var getToken = require('./secrets.js').GITHUB_TOKEN
+var getToken = require('./secrets.js').GITHUB_TOKEN // get from a file that is .gitignore
+
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -13,12 +15,37 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request(options, function(err, res, body) {
-    cb(err, body);
+request(options, function(err, res, body) {
+      var parsed = JSON.parse(body)
+      cb(err, parsed);{
+    }
   });
 }
 
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+  if (err) {
+    console.log("Errors:", err);
+  }
   console.log("Result:", result);
+var avatar = []
+
+ for(var i = 0; i < result.length; i++) {
+    avatar.push(result[i].avatar_url)
+    }
+      //console.log(avatar_url)
+    console.log ("avatar_url: " + avatar )
 });
+
+function downloadImageByURL(url, filePath) {
+  // ...
+}
+/*
+Result: { message: 'API rate limit exceeded for 69.174.3.130. (But here\'s the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)',
+  documentation_url: 'https://developer.github.com/v3/#rate-limiting' }
+avatar_url:
+*/
+// const
+
+//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+// avatar_url
